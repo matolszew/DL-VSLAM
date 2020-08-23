9ba0adc4678899f1acfc024d8afef01333f7ac74
Mateusz Olszewski <mateusz.olszewski@protonmail.com>
2020-08-24 00:46:29 +0200

    tracking function - not finished

--- orbslam.py
+++ orbslam.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
+import cv2
 from scipy.optimize import least_squares
 from scipy.spatial.transform import Rotation
+import multiprocessing
 
 from map import Map
 from key_frame import KeyFrame
+from covisibility_graph import CovisibilityGraph
 
 class ORBSLAM:
     """ORB-SLAM class
@@ -15,16 +18,24 @@ class ORBSLAM:
     Args:
         feature_detector (string):
     """
-    def __init__(self, camera_matrix, feature_detector='orb', initial_pose=None):
+    def __init__(self, camera_matrix, min_matches=100,
+                feature_detector='orb', initial_pose=None):
         self.camera_matrix = camera_matrix
         if feature_detector=='orb':
             self.feature_detector = cv2.ORB_create(nfeatures=500)
+        elif feature_detector=='mser':
+            self.feature_detector = cv2.MSER_create()
+        elif feature_detector=='fast':
+            self.feature_detector = cv2.FastFeatureDetector_create()
+        elif feature_detector=='surf':
+            self.feature_detector = cv2.SURF(400)
         else:
             raise ValueError('Not known feature detector')
+
         self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         self.match_treshold = 64
 
-        self.keyframes = []
+        self.graph = CovisibilityGraph(self.matcher, self.match_treshold)
         self.map = Map()
 
         self.initialize = True
@@ -36,6 +47,13 @@ class ORBSLAM:
             [0, 1, 0, 0],
             [0, 0, 1, 0]
         ]))
+        self.projection = np.copy(self.base_projection)
+        self.motion_model = self._calculate_velocity_model(self.projection)
+
+        self._last_key_frame = None
+        self._tracking_successful = False
+        self._global_relocalization_i = 0
+        self._key_frame_insertion_i = 0
 
     def update(self, img):
         """
@@ -43,35 +61,111 @@ class ORBSLAM:
         Args:
             img (cv2.):
         """
-        keypoints = self.feature_detector.detect(img, None)
-        keypoints, descriptors = self.feature_detector.compute(img, keypoints)
-        self.keyframes.append(KeyFrame(keypoints, descriptors))
+        keypoints, descriptors = self.feature_detector.detectAndCompute(img, None)
+        new_key_frame = KeyFrame(keypoints, descriptors)
 
         img2 = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0), flags=0)
         cv2.imshow('frame',img2)
         cv2.waitKey(1)
 
-        if len(self.keyframes) == 1:
+        if len(self.graph) == 0:
             # TODO: add possibility to initialize camera rotation and position
-            self.keyframes[0].update_rotation_from_quat([0,0,0,1])
-            self.keyframes[0].update_position(np.zeros((3,1)))
+            new_key_frame.update_rotation_from_quat([0,0,0,1])
+            new_key_frame.update_position(np.zeros((3,1)))
+            self.graph.add_node(new_key_frame)
+            self._last_key_frame = new_key_frame
             return
 
         if self.initialize:
-            self._initialize()
+            self._initialize(new_key_frame)
+        else:
+            self._tracking(new_key_frame)
 
-    def _initialize(self):
+        if len(self.keyframes) >= 5:
+            self.initialize = False
+
+
+    def _tracking(self, new_key_frame):
         """
         """
-        matches = self.matcher.match(self.keyframes[0].descriptors, self.keyframes[-1].descriptors)
+        if self.tracking_successful:
+            new_key_frame.update_position_and_rotation(np.matmul(self.motion_model, self.projection))
+
+            matches = self.matcher.match(new_key_frame.descriptors, self._last_key_frame.descriptors_in_map)
+            matches = [m for m in matches if m.distance < self.match_treshold]
+            ref_indicies = np.array([m.queryIdx for m in matches])
+            cur_indicies = np.array([m.trainIdx for m in matches])
+            map_indicies = self.graph[-1].map_indicies[ref_indicies]
+            new_key_frame.map_indicies[cur_indicies] = map_indicies
+
+            points3d = self.map.points3d[map_indicies]
+            pred_points2d = cv2.projectPoints(points3d,
+                                              new_key_frame.camera_rotation_vector,
+                                              new_key_frame.camera_position,
+                                              self.camera_matrix, None)
+            pred_error = np.sum(np.abs(new_key_frame.points2d[cur_indicies] - pred_points2d))
+
+            # TODO: Determine value when tracking is lost
+            if pred_error > 1e6:
+                self._tracking_successful = False
+            else:
+                self._motion_only_bundle_adjustment()
+                self._global_relocalization_i += 1
+
+        else:
+            # global relocalization
+            self._motion_only_bundle_adjustment()
+            self._global_relocalization_i = 0
+
+        # get all map points which could be visible in frame
+        # optimize pose with all of that points
+
+        # decide to add new keyframe or not
+        add_keyframe = True
+        if self._global_relocalization_i < 20:
+            add_keyframe = False
+        elif self._key_frame_insertion_i < 20: # or local mapping is stopped
+            add_keyframe = False
+        elif # less than 50 map points in the keyframe:
+            add_keyframe = False
+        elif # more than 90% of the same points in ref keyframe
+            add_keyframe = False
+
+        if add_keyframe:
+            self.graph.add_node(key_frame)
+            self._key_frame_insertion_i = 0
+        else:
+            self._key_frame_insertion_i += 1
+
+
+    def _local_mapping(self):
+        """
+        """
+        pass
+
+    def _loop_closing(self):
+        """
+        """
+        pass
+
+
+    def _initialize(self, new_key_frame):
+        """
+        """
+        matches = self.matcher.match(self.graph[0].descriptors, new_key_frame.descriptors)
         matches = [m for m in matches if m.distance < self.match_treshold]
+
+        if len(matches) < self.min_matches:
+            self.graph.reset()
+            self.graph.add_node(new_key_frame)
+            return
 
         ref_indicies = np.array([m.queryIdx for m in matches])
         cur_indicies = np.array([m.trainIdx for m in matches])
 
-        points_ref = self.keyframes[0].points2d[ref_indicies]
-        points_cur = self.keyframes[-1].points2d[cur_indicies]
-        description = self.keyframes[0].descriptors[ref_indicies]
+        points_ref = sself.graph[0].points2d[ref_indicies]
+        points_cur = new_key_frame.points2d[cur_indicies]
+        description = self.graph[0].descriptors[ref_indicies]
 
         # TODO: Calculate in parallel threads
         F, F_score, F_inliers = find_fundamental_matrix(points_ref, points_cur)
@@ -87,21 +181,34 @@ class ORBSLAM:
             cur_indicies = cur_indicies[F_inliers[:,0]]
             points3d, rt_matrix, inliers = self._position_from_fundamental(F, points_ref, points_cur)
             m_indices = self.map.add_points(points3d, description[inliers], self.matcher, self.match_treshold)
-            self.keyframes[0].update_indicies(m_indices, ref_indicies[inliers])
-            self.keyframes[-1].update_indicies(m_indices, cur_indicies[inliers])
-            self.keyframes[-1].update_position_and_rotation(rt_matrix)
+            self.graph[0].update_indicies(m_indices, ref_indicies[inliers])
+            new_key_frame.update_indicies(m_indices, cur_indicies[inliers])
+            new_key_frame.update_position_and_rotation(rt_matrix)
         else:
             # TODO: add calculation position from homography
             pass
 
+        self.graph.add_node(new_key_frame)
         self.bundle_adjustment()
+        self._last_key_frame = self.graph[-1]
+
+    def _calculate_velocity_model(self, projection):
+        """
+        """
+        projection = np.eye(4)
+        R = projection[:3,:3].T
+        T = -np.matmul(R, projection[:3,3])
+        projection[:3,:3] = R
+        projection[:3,3] = T
+
+        return projection
 
     def bundle_adjustment(self):
         """
         """
-        n = 6*len(self.keyframes) + 3*len(self.map)
+        n = 6*len(self.graph) + 3*len(self.map)
         x0 = np.empty((n))
-        for i, keyframe in enumerate(self.keyframes):
+        for i, keyframe in enumerate(self.graph):
             j = i*6
             x0[j:j+3] = keyframe.camera_rotation_vector
             x0[j+3:j+6] = keyframe.camera_position[:,0]
@@ -111,13 +218,17 @@ class ORBSLAM:
                             jac=self._calculate_jacobian, tr_solver='lsmr',
                             jac_sparsity=self._jacobian_sparsity(),
                             verbose=0)
-        for i, keyframe in enumerate(self.keyframes):
+        for i, keyframe in enumerate(self.graph):
             j = i*6
             keyframe.update_rotation_from_rotation_vector(res.x[j:j+3])
             keyframe.update_position(res.x[j+3:j+6])
             #print(res.x[j+3:j+6])
         self.map.update_points(res.x[-3*len(self.map):].reshape((len(self.map),3)))
         input("Press Enter to continue...")
+
+    def _motion_only_bundle_adjustment(self):
+        """
+        """
 
     def _jacobian_sparsity(self):
         """
